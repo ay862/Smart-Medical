798a8c9b395b0a85cc615640521e913be9977e94
mohamed-nasef <mnasef82@gmail.com>
2022-07-07 00:30:24 +0200

    remove pic get function

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const nurseSchedule = require("./routers/nurseScheduleRouters");
 const doctorSchedule = require("./routers/doctorScheduleRouters");
 const medicalRecord = require("./routers/medicalRecordRouters");
 const hospital = require("./routers/hospitalRouters");
-//const upload = require("./middleware/upload");
 const hospitalDoctor = require("./routers/hospitalDoctorRouters")
 const hospitalNurse = require("./routers/hospitalNurseRouters")
 const express = require("express");
@@ -33,22 +32,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-/*
-//we used upload.single to tell "multer" to upload
-// only single image 
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log(req.file);
-  res.send('Done');
-});
-*/
-
-app.use("/photo/:filename",function(req,res){
-  fs.readFile(`./uploads/${req.params.filename}`,(err, data)=>{
-      if (err) throw err; // Fail if the file can't be read.
-        res.writeHead(200, {'Content-Type': 'image/png || image/jpeg'});
-        res.end(data); // Send the file data to the browser.
-  });
-})
 
 app.use("/hospitaldoctor",hospitalDoctor);
 app.use("/hospitalnurse",hospitalNurse);
